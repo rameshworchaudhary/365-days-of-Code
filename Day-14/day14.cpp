45ca9e31e516855d64c9ad349a6c90abb028a513
rameshworchaudhary <194390492+rameshworchaudhary@users.noreply.github.com>
2026-09-04 15:57:17 +0530

    Day 14: Find Frequency of an Element

---
+++ Day-14/day14.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[10] = {10,20,10,30,10,20,30,40,50,60};
+    int target = 10;
+    int count = 0; 
+    for(int i =0;i<10;i++){
+        if(arr[i]==target){
+            count++;
+        }
+    }
+    cout<<"Count of "<<target<<" is: "<<count<<endl;
+    return 0;
+}
