fd56e9aa982866e2dd492532cc4d9bc0d2db7837
rameshworchaudhary <194390492+rameshworchaudhary@users.noreply.github.com>
2026-09-23 18:05:34 +0530

    Day 32 Completed in Python

--- Day-32/day32.cpp
+++ Day-32/day32.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-    int nums[6] = {6, 4, 8, 3, 5, 2};
-   
-    int RG = 0;
+    int nums[6] = {1,2,3,4,5,6};
+
 
     for(int i = 0; i < 6; i++){
-        RG = -1;
+        int RG = -1;
         for(int j = i + 1; j < 6; j++){
             if(nums[j] > nums[i]){
                 RG = nums[j];
                 break;
+        }
         }
         cout << RG << " ";
     }
--- Day-32/day32.py
+++ Day-32/day32.py
@@ -1,1 +1,9 @@
 nums = [1,2,3,5,6,7]
+
+for i in range(len(nums)):
+    RG = -1
+    for j in range(len(nums[i + 1:])):
+        if nums[j + i + 1] > nums[i]:
+            RG = nums[j + i + 1 ]
+            break
+    print(RG)
